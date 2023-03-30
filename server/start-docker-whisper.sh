fecd47f8dae62baed046fc73003a281b29fa5e29
Deepily <42214371+deepily@users.noreply.github.com>
2023-03-30 11:37:35 -0400

    Add debug switch to force reload upon file system refresh

--- server/start-docker-whisper.sh
+++ server/start-docker-whisper.sh
@@ -2,11 +2,15 @@
 
 if [ $# -eq 2 ]; then
 
-  echo "Starting docker whisper w/ image version [$2] from [$(pwd)]..."
+  echo "Starting docker whisper on port [$1] w/ image version [$2] from [$(pwd)]..."
   echo ""
   echo "Using OPENAI_API_KEY [$OPENAI_API_KEY]"
   echo ""
   echo "After Docker starts, run this: /var/genie-in-the-box/run-flask-whisper.sh"
+
+#  set RUN_THIS=/var/genie-in-the-box/run-flask-whisper.sh
+#  set RUN_THIS=bash
+
   docker container run -e OPENAI_API_KEY="$OPENAI_API_KEY" -e CUDA_DEVICE_ORDER=PCI_BUS_ID -e CUDA_VISIBLE_DEVICES=0,1 -e DOCKER_IMAGE_VERSION="$2" -it --volume /media/sdb/include/www.deepily.ai/projects:/var --publish 127.0.0.1:"$1":"$1"/tcp --runtime=nvidia --gpus all riqui/whisper:"$2" bash
 
 else
