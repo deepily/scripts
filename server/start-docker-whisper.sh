dd5e41684208db484b95106c344781e0ffafd151
Deepily <42214371+deepily@users.noreply.github.com>
2023-03-30 11:50:05 -0400

    Document removal of bash from invocation of docker

--- server/start-docker-whisper.sh
+++ server/start-docker-whisper.sh
@@ -8,9 +8,10 @@ if [ $# -eq 2 ]; then
   echo ""
   echo "After Docker starts, run this: /var/genie-in-the-box/run-flask-whisper.sh"
 
-#  set RUN_THIS=/var/genie-in-the-box/run-flask-whisper.sh
-#  set RUN_THIS=bash
+  #  set RUN_THIS=/var/genie-in-the-box/run-flask-whisper.sh
+  #  set RUN_THIS=bash
 
+  # ¡OJO! I removed 'bash' as the default startup command to run as the very last argument in this very long invocation.
   docker container run -e OPENAI_API_KEY="$OPENAI_API_KEY" -e CUDA_DEVICE_ORDER=PCI_BUS_ID -e CUDA_VISIBLE_DEVICES=0,1 -e DOCKER_IMAGE_VERSION="$2" -it --volume /media/sdb/include/www.deepily.ai/projects:/var --publish 127.0.0.1:"$1":"$1"/tcp --runtime=nvidia --gpus all riqui/whisper:"$2"
 
 else
